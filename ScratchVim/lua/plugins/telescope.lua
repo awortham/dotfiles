8405867b89d59946f7b32264b771ecfa8bbad154
Aaron Wortham <wortham.aaron@gmail.com>
2024-01-16 21:08:38 -0700

    Remove extra space

--- ScratchVim/lua/plugins/telescope.lua
+++ ScratchVim/lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
   {
     "nvim-telescope/telescope.nvim", tag = "0.1.5",
-    dependencies = { 
+    dependencies = {
       "nvim-lua/plenary.nvim",
       "cwebster2/github-coauthors.nvim"
     },
