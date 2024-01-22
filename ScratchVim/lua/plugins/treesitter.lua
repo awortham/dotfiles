e87d0dafa8eefbf04ea3fc4aa53d48905a48fa8c
Aaron Wortham <wortham.aaron@gmail.com>
2024-01-22 15:52:58 -0700

    Add auto install to tree sitter config

--- ScratchVim/lua/plugins/treesitter.lua
+++ ScratchVim/lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
     local configs = require("nvim-treesitter.configs")
 
     configs.setup({
-      ensure_installed = { "lua", "vim", "vimdoc", "javascript", "html" },
+      auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },  
     })
