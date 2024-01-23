caab57dad2febe4bdb4029589f5af0a4686af541
Aaron Wortham <wortham.aaron@gmail.com>
2024-01-23 08:18:14 -0700

    Format file with lua formatter

--- ScratchVim/lua/plugins/treesitter.lua
+++ ScratchVim/lua/plugins/treesitter.lua
@@ -1,13 +1,13 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local configs = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-    configs.setup({
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },  
-    })
-  end
+		configs.setup({
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
