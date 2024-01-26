306eabb5c1f181ccff4f8cd168af4b03180e6772
Aaron Wortham <wortham.aaron@gmail.com>
2024-01-26 14:23:30 -0700

    Formatting working except for ruby files :/

--- ScratchVim/lua/plugins/none-ls.lua
+++ ScratchVim/lua/plugins/none-ls.lua
@@ -1,11 +1,16 @@
 return {
 	"nvimtools/none-ls.nvim",
+  dependencies = { "mason.nvim" },
 	config = function()
 		local null_ls = require("null-ls")
 
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.diagnostics.standardrb,
+				null_ls.builtins.formatting.standardrb,
+				null_ls.builtins.diagnostics.eslint,
+				null_ls.builtins.formatting.prettier,
 			},
 		})
 
