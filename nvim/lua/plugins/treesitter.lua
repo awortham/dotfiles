6ab0d494c2bd564d74de680bb9580c570d66ae57
Aaron Wortham <wortham.aaron@gmail.com>
2024-02-02 18:06:52 -0700

    Comment out highlight setting

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,18 +1,20 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        auto_install = true,
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter-context",
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				auto_install = true,
+				sync_install = false,
+				-- commenting this option out make the gf command for vim-rails work.
+				-- I'm not yet sure if turning this off effects anything in treesitter
+				-- highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+	},
 }
