8966f3928e68ad2d4ef1be4b9b9a2c2ea1adc163
Aaron Wortham <wortham.aaron@gmail.com>
2024-02-08 09:58:43 -0700

    Auto Formatting

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,27 +1,27 @@
 local keymap = vim.keymap
 
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
 
-  config = function()
-    keymap.set("n", "<leader>e", ":Neotree filesystem reveal toggle left<CR>");
+	config = function()
+		keymap.set("n", "<leader>e", ":Neotree filesystem reveal toggle left<CR>")
 
-    -- I'd like to figure out how to get a more full path to files when moving them in the tree
-    require("neo-tree").setup({
-      event_handlers = {
-        {
-          event = "file_opened",
-          handler = function()
-            require("neo-tree").close_all()
-          end,
-        },
-      }
-    })
-  end
+		-- I'd like to figure out how to get a more full path to files when moving them in the tree
+		require("neo-tree").setup({
+			event_handlers = {
+				{
+					event = "file_opened",
+					handler = function()
+						require("neo-tree").close_all()
+					end,
+				},
+			},
+		})
+	end,
 }
