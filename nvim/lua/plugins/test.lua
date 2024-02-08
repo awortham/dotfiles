e0703ff2cac6eb092eea080cfa510a8546ccf139
Aaron Wortham <wortham.aaron@gmail.com>
2024-02-08 09:57:42 -0700

    Turn off spring when running specs

--- nvim/lua/plugins/test.lua
+++ nvim/lua/plugins/test.lua
@@ -1,16 +1,16 @@
 vim.g["test#strategy"] = "neovim"
-vim.g["test#ruby#use_binstubs"] = 1
-vim.g["test#ruby#use_spring_binstub"] = 1
+vim.g["test#ruby#use_binstubs"] = 0
+vim.g["test#ruby#use_spring_binstub"] = 0
 vim.g["test#preserve_screen"] = 1
 
 return {
-  {
-    "janko/vim-test",
-    keys = {
-      { "<leader>s", ":TestNearest<CR>" },
-      { "<leader>t", ":TestFile<CR>" },
-      { "<leader>a", ":TestSuite<CR>" },
-      { "<CR>", ":TestLast<CR>" },
-    },
-  },
+	{
+		"janko/vim-test",
+		keys = {
+			{ "<leader>s", ":TestNearest<CR>" },
+			{ "<leader>t", ":TestFile<CR>" },
+			{ "<leader>a", ":TestSuite<CR>" },
+			{ "<CR>", ":TestLast<CR>" },
+		},
+	},
 }
