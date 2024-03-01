2b59e7e1fc3bbd27cccdf4ed5aa5f1461810c3b4
Aaron Wortham <wortham.aaron@gmail.com>
2024-03-01 13:28:59 -0700

    Add treesitter highlighting back

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -8,6 +8,11 @@ return {
       config.setup({
         auto_install = true,
         sync_install = false,
+        highlight = {
+          enable = true,
+          -- https://github.com/nvim-treesitter/nvim-treesitter/issues/6187
+          additional_vim_regex_highlighting = true,
+        },
         indent = { enable = true },
       })
     end,
